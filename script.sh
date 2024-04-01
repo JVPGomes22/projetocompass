b3f89c843d17227fcab1516803094f76f8726817
João Victor Gomes <92761247+JvCarioca@users.noreply.github.com>
2024-04-01 12:02:48 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ DATA=$(date +"%d/%m/%y - %H:%M:%S")
 
 # Verifica se o arquivo "exemplo.txt" existe
 if [ $STATUS = "inactive" ]; then
-    echo "APACHE - ${DATA} Offline"  >> /srv/nfs/shared/joao-victor/offline.txt
+    echo "APACHE - ${DATA} Offline"  >> /nfs-aws/joao-victor/offline.txt
 else
-    echo "APACHE - ${DATA} Online" >> /srv/nfs/shared/joao-victor/online.txt
+    echo "APACHE - ${DATA} Online" >> /nfs-aws/joao-victor/online.txt
 fi
